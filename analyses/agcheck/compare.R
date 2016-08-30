b8fda427630d17191b607b9b2f8de2ea17423bd2
James Rising <jarising@gmail.com>
2016-08-29 23:54:11 -0700

    add new other crops

--- analyses/agcheck/compare.R
+++ analyses/agcheck/compare.R
@@ -5,12 +5,12 @@ dfbycy <- read.csv("byyear.csv")
 library(ggplot2)
 
 df1 <- data.frame(crop=rep(dfbycy$crop, 4), isobs=rep(c(T, F, T, F), each=nrow(dfbycy)), isirrig=rep(c(T, T, F, F), each=nrow(dfbycy)), yield=c(dfbycy$obsirrigatedyield, dfbycy$estirrigatedyield, dfbycy$obsrainfedyield, dfbycy$estrainfedyield))
-df2 <- data.frame(crop=dfbycy$crop, obsyield=c(dfbycy$obsirrigatedyield[dfbycy$crop == 'soybeans'], dfbycy$obsrainfedyield[dfbycy$crop %in% c('barley', 'hay', 'sorghum')]), estyield=c(dfbycy$estirrigatedyield[dfbycy$crop == 'soybeans'], dfbycy$estrainfedyield[dfbycy$crop %in% c('barley', 'hay', 'sorghum')]))
+df2 <- data.frame(crop=c(as.character(dfbycy$crop[dfbycy$crop == 'soybeans']), as.character(dfbycy$crop[dfbycy$crop %in% c('barley', 'hay', 'sorghum')]), paste0('irrigated', dfbycy$crop[dfbycy$crop %in% c('maize', 'wheat')]), paste0('rainfed', dfbycy$crop[dfbycy$crop %in% c('maize', 'wheat')])), obsyield=c(dfbycy$obsirrigatedyield[dfbycy$crop == 'soybeans'], dfbycy$obsrainfedyield[dfbycy$crop %in% c('barley', 'hay', 'sorghum')], dfbycy$obsirrigatedyield[dfbycy$crop %in% c('maize', 'wheat')], dfbycy$obsrainfedyield[dfbycy$crop %in% c('maize', 'wheat')]), estyield=c(dfbycy$estirrigatedyield[dfbycy$crop == 'soybeans'], dfbycy$estrainfedyield[dfbycy$crop %in% c('barley', 'hay', 'sorghum')], dfbycy$estirrigatedyield[dfbycy$crop %in% c('maize', 'wheat')], dfbycy$estrainfedyield[dfbycy$crop %in% c('maize', 'wheat')]))
 
 df2$obsyield[df2$obsyield == -1] <- NA
 
 ggplot(df2, aes(x=obsyield, y=estyield)) +
-    facet_wrap(~ crop, scales="free") +
+    facet_wrap(~ crop, scales="free", ncol=4) +
     geom_point(alpha=.1) + stat_smooth(method=lm) +
     theme_bw() + xlab("Observed Yield") + ylab("Estimated Yield")
 

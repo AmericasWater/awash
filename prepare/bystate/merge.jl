834ced90dc0cb70c57c7d2d5bca7bc76a919aa8b
James Rising <jarising@gmail.com>
2016-09-19 15:18:58 -0700

    first version of merger

---
+++ prepare/bystate/merge.jl
@@ -0,0 +1,96 @@
+include("../../src/lib/readconfig.jl")
+suffix = ""
+config = emptyconfig()
+
+include("../../src/lib/datastore.jl")
+include("../../src/lib/reservoirs.jl")
+include("../../src/waternet.jl")
+
+waternetdata = rdataload(datapath("waternet.RData"));
+netdata = waternetdata["network"];
+
+drawsdata = rdataload(datapath("countydraws.RData"));
+draws = drawsdata["draws"];
+draws[:source] = round(Int64, draws[:source])
+
+# Label all with the node name
+draws[:gaugeid] = ""
+for ii in 1:nrow(draws)
+    row = draws[ii, :source]
+    draws[ii, :gaugeid] = "$(netdata[row, :collection]).$(netdata[row, :colid])"
+end
+
+mastercounties = readtable(datapath("global/counties$suffix.csv"), eltypes=[UTF8String, UTF8String, UTF8String])
+
+getregion(label) = round(Int64, floor(draws[(draws[:gaugeid] .== label) & (draws[:justif] .== "contains"), :source] / 1000))
+allregions() = unique(map(fips -> fips[1:2], mastercounties[:state]))
+
+outedges = Dict{ExVertex, Vector{ExVertex}}() # all destined newwaternet nodes (but some might be dropped for region)
+merging = Dict{ExVertex, ExVertex}(); # feed runoff from waternet A to newaternet B
+
+index = 0
+for hh in length(downstreamorder):-1:1 # start at most downstream
+    node = downstreamorder[hh]
+
+    # Has this already been merged into a group node?
+    if in(node, keys(merging))
+        newnode = merging[node]
+    else
+        newnode = ExVertex(index += 1, label(node))
+        outedges[newnode] = []
+    end
+    
+    upstreams = out_neighbors(node, waternet)
+    if length(upstreams) > 0
+        # Group into sets in the same region
+        byregion = Dict{UTF8String, Vector{ExVertex}}()
+        for upstream in upstreams
+            region = getregion(label(upstream))
+            if !in(region, keys(byregion))
+                byregion[region] = []
+            end
+            push!(byregion[region], upstream)
+        end
+
+        # Are these all in the same region?
+        if length(byregion) == 1
+            # Merge them all together
+            for upstream in upstreams
+                merging[upstream] = newnode
+            end
+        else
+            for region, upstreams in byregion
+                regionnode = ExVertex(index += 1, label(upstreams[1]))
+                for upstream in upstreams
+                    merging[upstream] = regionnode
+                end
+                push!(outedges[newnode], regionnode)
+            end
+        end
+    end
+end
+
+# Now construct the final network
+
+newwateridverts = Dict{UTF8String, ExVertex}();
+newwaternet = empty_extnetwork();
+
+# Add node for each region
+for region in allregions()
+    newwateridverts[region] = ExVertex(length(newwateridverts), region)
+    add_vertex!(newwaternet, newwateridverts[region])
+end
+
+for newnode, outnewnodes in outedges
+    if length(outnewnodes) == 0
+        # Replace this merged region node with the whole state
+        for node in keys(merging)
+            if merging[node] == newnode
+                merging[node] = newwateridverts[getregion(label(node))]
+            end
+        end
+    else
+        newwateridverts[label(newnode)] = newnode
+        add_vertex!(newaternet, newnode)
+    end
+end

ce16a99f9a79ec1855bc541865e99b20e586a14c
James Rising <jarising@gmail.com>
2016-09-28 18:18:57 -0400

    load config

--- test/test_optimize_surface.jl
+++ test/test_optimize_surface.jl
@@ -1,5 +1,12 @@
 using Base.Test
 
+using DataArrays
+using DataFrames
+using OptiMimi
+
+include("../src/lib/readconfig.jl")
+config = readconfig("../configs/standard-1year.yml")
+
 include("../src/optimization-given.jl")
 
 house = optimization_given(false)

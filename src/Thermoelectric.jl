3dd5f008f036707526bc645822d805a67d3ea02e
Sophia <sophia.joohye@gmail.com>
2016-10-03 13:46:26 -0400

    typos

--- src/Thermoelectric.jl
+++ src/Thermoelectric.jl
@@ -35,7 +35,7 @@ function initthermoelectric(m::Model)
     thermoelectric = addcomponent(m, Thermoelectric)
 
     recorded = readtable(datapath("extraction/USGS-2010.csv"))
-    thermoelectric[:demand] = repeat(convert(Vector, recorded[:, :PT_To] * 1382592. / (1000. * config["timestep"])), outer=[1, numsteps])
+    thermoelectric[:demand] = repeat(convert(Vector, recorded[:, :PT_To] * 1382592. / (1000.*12) * config["timestep"]), outer=[1, numsteps])
 
     thermoelectric
 end

bf8f54ecce1b1a5da37062835f0856f3c1af0fbf
James Rising <jarising@gmail.com>
2016-10-13 09:46:14 -0500

    rolling precip calculation

--- src/Agriculture.jl
+++ src/Agriculture.jl
@@ -130,7 +130,11 @@ function initagriculture(m::Model)
     agriculture[:logirrigatedyield] = logirrigatedyield
     agriculture[:deficit_coeff] = deficit_coeff
     agriculture[:water_demand] = water_demand
-    agriculture[:precipitation] = precip
+
+    # Sum precip to a yearly level
+    stepsperyear = floor(Int64, 12 / config["timestep"])
+    rollingsum = cumsum(precip, 2) - cumsum([zeros(numregions, stepsperyear) precip[:, 1:size(precip)[2] - stepsperyear]])
+    agriculture[:precipitation] = rollingsum
 
     # Load in planted area by water management
     rainfeds = readtable(joinpath(todata, "agriculture/rainfedareas.csv"))

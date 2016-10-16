f8a95c1e3758f122d44ebeeac36e0d127f17d259
James Rising <jarising@gmail.com>
2016-10-15 17:21:38 -0700

    consistent and silent

--- analyses/optradii/holdstate.jl
+++ analyses/optradii/holdstate.jl
@@ -3,7 +3,7 @@ include("lib/readconfig.jl")
 config = readconfig("../configs/standard-1year.yml") # Just use 1 year for optimization
 
 include("optimization-given.jl")
-house = optimization_given(true)
+house = optimization_given(true);
 
 using MathProgBase
 using Gurobi

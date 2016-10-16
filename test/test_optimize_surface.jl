7db309e7384b070baeb7bd1fc84834ccb7883ded
James Rising <jarising@gmail.com>
2016-10-16 10:28:57 -0700

    generalize paths

--- test/test_optimize_surface.jl
+++ test/test_optimize_surface.jl
@@ -20,12 +20,12 @@ solver = ClpSolver()
 # Save the results, if not available
 varlens = varlengths(house.model, house.paramcomps, house.parameters)
 
-if !isfile("../data/extraction/withdrawals$suffix.jld")
-    serialize(open("../data/extraction/withdrawals$suffix.jld", "w"), reshape(sol.sol[varlens[1]+1:sum(varlens[1:2])], numcanals, numsteps))
+if !isfile(datapath("extraction/withdrawals$suffix.jld"))
+    serialize(open(datapath("extraction/withdrawals$suffix.jld"), "w"), reshape(sol.sol[varlens[1]+1:sum(varlens[1:2])], numcanals, numsteps))
 end
-if !isfile("../data/extraction/returns$suffix.jld")
-    serialize(open("../data/extraction/returns$suffix.jld", "w"), reshape(sol.sol[sum(varlens[1:2])+1:sum(varlens[1:3])], numcanals, numsteps))
+if !isfile(datapath("extraction/returns$suffix.jld"))
+    serialize(open(datapath("extraction/returns$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:2])+1:sum(varlens[1:3])], numcanals, numsteps))
 end
-if !isfile("../data/extraction/captures$suffix.jld")
-    serialize(open("../data/extraction/captures$suffix.jld", "w"), reshape(sol.sol[sum(varlens[1:3])+1:end], numreservoirs, numsteps))
+if !isfile(datapath("extraction/captures$suffix.jld"))
+    serialize(open(datapath("extraction/captures$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:3])+1:end], numreservoirs, numsteps))
 end

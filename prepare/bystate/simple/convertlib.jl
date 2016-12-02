96699a46390b24b54ada67d782b779e25007738f
James Rising <jarising@gmail.com>
2016-12-01 21:57:12 -0500

    conversion of agriculture to state level

---
+++ prepare/bystate/simple/convertlib.jl
@@ -0,0 +1,175 @@
+using DataFrames
+
+function translateregion(subdf, columns, todrop, translate)
+    # Add on each column
+    for name in names(subdf)
+        value = translate(name, subdf[name])
+        if !isna(value) && value == nothing
+            push!(todrop, name)
+        else
+            if !in(name, keys(columns))
+                if isna(value)
+                    columns[name] = DataVector{Float64}([])
+                else
+                    columns[name] = DataVector{typeof(value)}([])
+                end
+            end
+            push!(columns[name], value)
+        end
+    end
+end
+
+todata = "../../.."
+
+"""
+Convert a table by passing each super-region's values for each column to `translate`
+
+Configuration parameters (all symbols of the `config` dictionary):
+- `source`: dataset of source files (e.g., counties)
+- `target`: dataset of target files (e.g., states)
+- `sourceid`: The source ID column in the source file
+- `forcematching`: should the number of rows match the master file?  true/false
+- `mastersourceid`: The source ID column in the master file
+"""
+function converttable(filename, config, translate)
+    println("Reading data...")
+
+    masterregions = readtable(joinpath(todata, config[:masterfile]),
+                              eltypes=[UTF8String, UTF8String, UTF8String])
+
+    # Check that we have all regions
+    df = readtable(joinpath(todata, "data", config[:source], filename))
+    if nrow(df) != nrow(masterregions)
+        if config[:forcematching]
+            error("Rows don't match: $(nrow(df)) <> $(nrow(masterregions))")
+        else
+            warn("Rows don't match: $(nrow(df)) <> $(nrow(masterregions))")
+        end
+    end
+
+    # Prepare the destination
+    mkpath(joinpath(todata, "data", config[:target], dirname(filename)))
+
+    # Construct the result, one region at a time
+    columns = Dict{Symbol, Any}()
+    todrop = Set{Symbol}()
+    for region in unique(masterregions[config[:mastertargetid]])
+        println("  $region")
+        # Collect all the regions
+        subregions = masterregions[masterregions[config[:mastertargetid]] .== region, config[:mastersourceid]]
+        subregions = map(id -> parse(Int64, id), subregions)
+        subdf = df[convert(Vector{Bool}, map(subreg -> in(subreg, subregions), df[config[:sourceid]])), :]
+
+        translateregion(subdf, columns, todrop, translate)
+    end
+
+    result = DataFrame()
+    result[config[:targetid]] = unique(masterregions[config[:mastertargetid]])
+    for name in filter(name -> !in(name, todrop), names(df))
+        result[name] = columns[name]
+    end
+
+    writetable(joinpath(todata, "data", config[:target], filename), result)
+end
+
+"""
+Configuration parameters (all symbols of the `config` dictionary):
+- `source`: dataset of source files (e.g., counties)
+- `target`: dataset of target files (e.g., states)
+- `mastersourcefile`: master file with all source regions
+- `mastertargetfile`: master file with all target regions
+- `mastersourceid`: column in source master file with target region ids
+- `mastertargetid`: column in target master file with target region ids
+"""
+function orderedconverttable(filename, config, translate)
+    println("Reading data...")
+
+    sourceregions = readtable(joinpath(todata, config[:mastersourcefile]),
+                              eltypes=[UTF8String, UTF8String, UTF8String])
+    targetregions = readtable(joinpath(todata, config[:mastertargetfile]),
+                              eltypes=[UTF8String, UTF8String, UTF8String])
+
+    # Check that we have all regions
+    df = readtable(joinpath(todata, "data", config[:source], filename))
+    if nrow(df) != nrow(sourceregions)
+        error("Rows don't match: $(nrow(df)) <> $(nrow(sourceregions))")
+    end
+
+    # Prepare the destination
+    mkpath(joinpath(todata, "data", config[:target], dirname(filename)))
+
+    # Construct the result, one region at a time
+    columns = Dict{Symbol, Any}()
+    todrop = Set{Symbol}()
+    for region in targetregions[config[:mastertargetid]]
+        println("  $region")
+        # Collect all the regions
+        subdf = df[sourceregions[config[:mastersourceid]] .== region, :]
+
+        if nrow(subdf) > 0
+            translateregion(subdf, columns, todrop, translate)
+        end
+    end
+
+    result = DataFrame()
+    for name in filter(name -> !in(name, todrop), names(df))
+        result[name] = columns[name]
+    end
+
+    writetable(joinpath(todata, "data", config[:target], filename), result)
+end
+
+"""
+Configuration-based direct copy
+"""
+function mirrorfile(filename, config)
+    src = joinpath(todata, "data", config[:source], filename)
+    dst = joinpath(todata, "data", config[:target], filename)
+
+    mkpath(dirname(dst))
+
+    cp(src, dst, remove_destination=true)
+end
+
+"""
+Convert a table by passing each super-region sub-table to translate
+
+Configuration parameters (all symbols of the `config` dictionary):
+- `source`: dataset of source files (e.g., counties)
+- `target`: dataset of target files (e.g., states)
+- `sourceid`: The source ID column in the source file
+- `mastersourceid`: The source ID column in the master file
+"""
+function chunkyconverttable(filename, config, translatechunk)
+    println("Reading data...")
+
+    masterregions = readtable(joinpath(todata, config[:masterfile]),
+                              eltypes=[UTF8String, UTF8String, UTF8String])
+
+    # Check that we have all regions
+    df = readtable(joinpath(todata, "data", config[:source], filename))
+
+    # Prepare the destination
+    mkpath(joinpath(todata, "data", config[:target], dirname(filename)))
+
+    # Construct the result, one region at a time
+    result = nothing
+    for region in unique(masterregions[config[:mastertargetid]])
+        println("  $region")
+        # Collect all the regions
+        subregions = masterregions[masterregions[config[:mastertargetid]] .== region, config[:mastersourceid]]
+        subregions = map(id -> parse(Int64, id), subregions)
+        subdf = df[convert(Vector{Bool}, map(subreg -> in(subreg, subregions), df[config[:sourceid]])), :]
+
+        subresult = translatechunk(subdf)
+        subresult[config[:targetid]] = region
+
+        if result == nothing
+            result = subresult
+        else
+            append!(result, subresult)
+        end
+    end
+
+    writetable(joinpath(todata, "data", config[:target], filename), result)
+end

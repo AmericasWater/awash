2edec285ff4bc84114ddbd7b1b338c290b3ca20e
James Rising <jarising@gmail.com>
2016-12-08 14:04:36 -0800

    converted reservoirs to statelevel

--- prepare/bystate/waternet/merge-reservoirs.jl
+++ prepare/bystate/waternet/merge-reservoirs.jl
@@ -35,17 +35,40 @@ function findnode(nodename)
     return findnode(foundvertex.label)
 end
 
-outlets = Dict{UTF8String, Vector{UTF8String}}()
+outlets = Dict{UTF8String, Vector{Int64}}()
 for ii in 1:nrow(allreservoirs)
     label = "$(allreservoirs[ii, :collection]).$(allreservoirs[ii, :colid])"
     println("$ii: $label ($(length(keys(outlets))))")
     found = findnode(label)
     if found != nothing
         if !in(found, keys(outlets))
-            outlets[found] = UTF8String[]
+            outlets[found] = Int64[]
         end
-        push!(outlets[found], label)
+        push!(outlets[found], ii)
     end
 end
 
-println(outlets)
+network = load(datapath("waternet.RData"))["network"];
+network[:label] = map(ii -> "$(network[ii, :collection]).$(network[ii, :colid])", 1:nrow(network))
+
+states = readtable(datapath("global/states.csv"))
+
+result = DataFrame(collection=UTF8String[], colid=UTF8String[], area=Float64[],
+                   lat=Float64[], lon=Float64[], elev=Float64[], MAXCAP=Float64[], ST=UTF8String[])
+for label in keys(outlets)
+    colcolid = split(label, ".")
+    networkrow = network[:label] .== label
+
+    # Find state most common
+    allstatefips = map(fips -> string(div(fips, 1000)), allreservoirs[outlets[label], :fips])
+    countstatefips = countmap(allstatefips)
+    maxval, maxkey = findmax(countstatefips)
+    statefips = parse(Int, maxval[1])
+    
+    push!(result, [colcolid; sum(allreservoirs[outlets[label], :area]);
+                   network[networkrow, :lat]; network[networkrow, :lon];
+                   NA; sum(allreservoirs[outlets[label], :MAXCAP]);
+                   states[states[:fips] .== statefips, :state]])
+end
+
+writetable(datapath("states/reservoirs/allreservoirs.csv"), result)

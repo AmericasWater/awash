83b416d25a602cb771fa286df3df0d96cf76003a
James Rising <jarising@gmail.com>
2017-04-18 13:26:44 -0400

    remove this code

--- src/optimize.jl
+++ src/optimize.jl
@@ -12,61 +12,66 @@ include("optimization.jl")
 using MathProgBase
 @time sol = linprog(-house.f, house.A, '<', house.b, house.lowers, house.uppers)
 
-coning = constraining(house, convert(Vector{Float64}, sol.sol))
+debug = false
 
-rdf = DataFrame(fips=masterregions[:fips]);
-cdf = DataFrame(fips=repmat(masterregions[:fips], numallcrops), crop=vec(repeat(crops, inner=[numcounties, 1])));
+if debug
+    coning = constraining(house, convert(Vector{Float64}, sol.sol))
 
-# Look at parameter values
-varlens = varlengths(m, house.paramcomps, house.parameters)
-for ii in 1:length(house.parameters)
-    println(house.parameters[ii])
-    index1 = sum(varlens[1:ii-1]) + 1
-    index2 = sum(varlens[1:ii])
+    rdf = DataFrame(fips=masterregions[:fips]);
+    cdf = DataFrame(fips=repmat(masterregions[:fips], numallcrops), crop=vec(repeat(allcrops, inner=[numcounties, 1])));
 
-    values = sol.sol[index1:index2]
+    # Look at parameter values
+    varlens = varlengths(m, house.paramcomps, house.parameters)
+    for ii in 1:length(house.parameters)
+        println(house.parameters[ii])
+        index1 = sum(varlens[1:ii-1]) + 1
+        index2 = sum(varlens[1:ii])
 
-    if varlens[ii] == nrow(rdf)
-        rdf[house.parameters[ii]] = values
-    elseif varlens[ii] == nrow(cdf)
-        cdf[house.parameters[ii]] = values
-    else
-        #println("Cannot store: $(varlens[ii])")
+        values = sol.sol[index1:index2]
+
+        if varlens[ii] == nrow(rdf)
+            rdf[house.parameters[ii]] = values
+        elseif varlens[ii] == nrow(cdf)
+            cdf[house.parameters[ii]] = values
+        else
+            #println("Cannot store: $(varlens[ii])")
+        end
+
+        if (sum(values .!= 0) == 0)
+            println("All zero.")
+        else
+            println(values[1:min(100, index2 - index1 + 1)])
+            println("Sum: $(sum(values))")
+        end
     end
 
-    if (sum(values .!= 0) == 0)
-        println("All zero.")
-    else
-        println(values[1:min(100, index2 - index1 + 1)])
-        println("Sum: $(sum(values))")
+    # Get constraint values
+    constvalues = house.A * sol.sol
+
+    varlens = varlengths(m, house.constcomps, house.constraints)
+    for ii in 1:length(house.constraints)
+        println(house.constraints[ii])
+        index1 = sum(varlens[1:ii-1]) + 1
+        index2 = sum(varlens[1:ii])
+
+        values = constvalues[index1:index2]
+
+        if varlens[ii] == nrow(rdf)
+            rdf[house.constraints[ii]] = values
+        elseif varlens[ii] == nrow(cdf)
+            cdf[house.constraints[ii]] = values
+        else
+            #println("Cannot store: $(varlens[ii])")
+        end
+
+        if (sum(values .!= 0) == 0)
+            println("All zero.")
+        else
+            println(values[1:min(100, index2 - index1 + 1)])
+        end
     end
+
+    writetable("../results/regionout.csv", rdf)
+    writetable("../results/cropsout.csv", cdf)
 end
 
-# Get constraint values
-constvalues = house.A * sol.sol
-
-varlens = varlengths(m, house.constcomps, house.constraints)
-for ii in 1:length(house.constraints)
-    println(house.constraints[ii])
-    index1 = sum(varlens[1:ii-1]) + 1
-    index2 = sum(varlens[1:ii])
-
-    values = constvalues[index1:index2]
-
-    if varlens[ii] == nrow(rdf)
-        rdf[house.constraints[ii]] = values
-    elseif varlens[ii] == nrow(cdf)
-        cdf[house.constraints[ii]] = values
-    else
-        #println("Cannot store: $(varlens[ii])")
-    end
-
-    if (sum(values .!= 0) == 0)
-        println("All zero.")
-    else
-        println(values[1:min(100, index2 - index1 + 1)])
-    end
-end
-
-writetable("../results/regionout.csv", rdf)
-writetable("../results/cropsout.csv", cdf)

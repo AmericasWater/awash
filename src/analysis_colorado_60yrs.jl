2d8baaa5d086a7a939c1ca8be20f313dd7a7cced
Sophia <sophia.joohye@gmail.com>
2017-10-03 14:28:44 -0400

    analysis added

--- src/analysis_colorado_60yrs.jl
+++ src/analysis_colorado_60yrs.jl
@@ -3,10 +3,97 @@ using RCall
 include("lib/graphing-rcall.jl")
 
 R_barplot = R"barplot" 
-df=readtable(datapath("Colorado/sample.csv")); #sample dataframe 
+df=readtable(datapath("../Colorado/sample.csv")); #sample dataframe 
+rename!(df, :x, :value)
+
+year=[1951:2010]
+
+#Map Area
+area=getdata(:UnivariateAgriculture,:totalareas)[:,:,1]
+df[:value]=sum(area,2)[:,1];
+usmap_colorado(df,true)
+
+#Map crops (NO Hay, Wheat irrigated, soybeans (5,7,8))
+df[:value]=area[:,1]  #1,2,3,4,6
+usmap_colorado(df,true)
+
+#MAP Mean Revenue
+revenue=getdata(:Market,:domesticrevenue);
+total_revenue=sum(sum(revenue,2),3)[:,1];
+df[:value]=total_revenue;
+usmap_colorado(df,true)
+
+
+
+#Map Total Water Use over 60 yrs
+sw=getdata(:Allocation,:swsupply);
+df[:value]=sum(sw,2)[:,1];
+usmap_colorado(df,true)
+gw=getdata(:Allocation,:waterfromgw);
+df[:value]=sum(gw,2)[:,1];
+usmap_colorado(df,true)
+totals=sw+gw;
+df[:value]=sum(totals,2)[:,1];
+usmap_colorado(df,true)
+
+#Money per Water Use 
+df[:value]= total_revenue./sum(totals,2)[:,1]
+usmap_colorado(df,true)
+
+
+#Map unit water cost 
+swcost=getdata(:Allocation,:swcost)
+df[:value]=sum(swcost,2)[:,1]./sum(sw,2)[:,1]
+usmap_colorado(df,true)
+gwcost=getdata(:Allocation,:cost)-getdata(:Allocation,:swcost)
+df[:value]=sum(gwcost,2)[:,1]./sum(gw,2)[:,1]
+usmap_colorado(df,true)
+df[:value]=sum(swcost,2)[:,1]
+usmap_colorado(df,true)
+df[:value]=sum(gwcost,2)[:,1]
+usmap_colorado(df,true)
+
+
+
+
+opcost=getdata(:UnivariateAgriculture,:opcost);
+
+df3=DataFrame(year=year,sw=reshape(sum(sw,1),60,1)[:,1],gw=reshape(sum(gw,1),60,1)[:,1],
+swcost=reshape(sum(swcost,1),60,1)[:,1],gwcost=reshape(sum(gwcost,1),60,1)[:,1],
+revenue=reshape(sum(sum(revenue,1),2),60,1,1)[:,1],opcost=reshape(sum(sum(opcost,1),2),60,1,1)[:,1],
+barley=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,1,:],1),60,1,1)[:,1],
+corn=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,2,:],1),60,1,1)[:,1]+
+reshape(sum(getdata(:UnivariateAgriculture,:production)[:,3,:],1),60,1,1)[:,1],
+sorghum=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,4,:],1),60,1,1)[:,1],
+soybean=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,5,:],1),60,1,1)[:,1],
+wheat=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,6,:],1),60,1,1)[:,1]+
+    reshape(sum(getdata(:UnivariateAgriculture,:production)[:,7,:],1),60,1,1)[:,1],
+hay=reshape(sum(getdata(:UnivariateAgriculture,:production)[:,8,:],1),60,1,1)[:,1],
+precip=reshape(sum(precip,1),60,1)[:,1])
+writetable(datapath("../../sim.csv"), df3)
+
+
+
+
+
+
+
+
+
+
+
 delete!(df,:fips);#Analysis for Colorado 
 delete!(df,:x);
+
+
+
 #Map1: Irrigated Area
+
+
+
+
+
+
 
 function clear
     df=readtable(datapath("Colorado/sample.csv")); #sample dataframe 
@@ -86,7 +173,7 @@ clear
 #revenue
 revenue=getdata(:Market,:domesticrevenue);
 for i in 1:60
-    df=hcat(df,revenue[:,i])
+    df1=hcat(df,revenue[:,i])
 end 
 writetable(datapath("../results/Colorado60/revenue.csv"), df)
 clear

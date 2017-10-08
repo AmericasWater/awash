26dc02f326c84f0acde99356b8879567a7434730
James Rising <jarising@gmail.com>
2017-10-08 09:59:39 -0500

    only save some of ambiguous

--- test/test_optimize_surface.jl
+++ test/test_optimize_surface.jl
@@ -20,33 +20,41 @@ solver = ClpSolver()
 # Save the results, if not available
 varlens = varlengths(house.model, house.paramcomps, house.parameters)
 
-allparams = []
+allparams = Symbol[]
+allvalues = Float64[]
 for ii in 1:length(house.parameters)
-    allparams = [allparams; repeat([house.parameters[ii]], outer=[varlens[ii]])]
+    if house.parameters[ii] in [:withdrawals, :returns]
+        # Just include sum, since not completely constrained
+        allparams = [allparams; house.parameters[ii]]
+        allvalues = [allvalues; sum(sol.sol[sum([1; varlens[1:(ii-1)]]):sum(varlens[1:ii])])]
+    else
+        allparams = [allparams; repeat([house.parameters[ii]], outer=[varlens[ii]])]
+        allvalues = [allvalues; sol.sol[sum([1; varlens[1:(ii-1)]]):sum(varlens[1:ii])]]
+    end
 end
-alldf = DataFrame(parameter=allparams, value=sol.sol)
+alldf = DataFrame(parameter=allparams, value=allvalues)
 
 outputpath = joinpath(dirname(@__FILE__), "../results/optimize-surface-test.csv")
 if isfile(outputpath)
     compdf = readtable(outputpath)
     @test nrow(compdf) == nrow(alldf)
-    mismatches = []
+    mismatches = Int64[]
     for ii in 1:nrow(alldf)
         @test compdf[ii, :parameter] == string(alldf[ii, :parameter])
         if compdf[ii, :value] != alldf[ii, :value]
             push!(mismatches, ii)
         end
     end
+
+    if length(mismatches) > 0
+        println(mismatches)
+        println(alldf[mismatches, :])
+        println(compdf[mismatches, :])
+    end
+    @test length(mismatches) == 0
 else
     writetable(outputpath, alldf)
 end
-
-if length(mismatches) > 0
-    println(mismatches)
-    println(alldf[mismatches, :])
-    println(compdf[mismatches, :])
-end
-@test length(mismatches) == 0
 
 if !isfile(datapath("extraction/withdrawals$suffix.jld"))
     serialize(open(datapath("extraction/withdrawals$suffix.jld"), "w"), reshape(sol.sol[varlens[1]+1:sum(varlens[1:2])], numcanals, numsteps))

64dad3700c1203bafea1b0b184f9be4cbeaf313a
James Rising <jarising@gmail.com>
2018-02-09 22:16:16 +0000

    should include lib/datastore

--- src/watercostdata.jl
+++ src/watercostdata.jl
@@ -1,6 +1,8 @@
 using DataFrames
 using RData
 
+include("lib/datastore.jl")
+
 # energy cost to lift 1000m3 by 1m
 #energycostperlift = 1.
 
@@ -10,9 +12,9 @@ energycostperlift = 0.423133696 # in $/1000m3 /m of lift | value from CALVIN was
 ### EXTRACTION COST
 # sw: compute relative elevation if source downhill, 0 otherwise
 # if missing information, default value is specified by naelev
-if isfile(datapath("canalextractioncost$suffix.jld"))
+if isfile(loadpath("canalextractioncost$suffix.jld"))
     println("Loading extraction cost from saved data...")
-    canalextractioncost = deserialize(open(datapath("canalextractioncost$suffix.jld"), "r"))*energycostperlift;
+    canalextractioncost = deserialize(open(loadpath("canalextractioncost$suffix.jld"), "r"))*energycostperlift;
 else
     ## Optional cost for drawing down a river (environmental change)
     # Marginal cost is $3178.73 / MG, but 92% not subject to treatment costs, so $248.53 / MG

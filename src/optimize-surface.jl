448edf136c2952d8aba3429b6befb9eda1ea4ce7
James Rising <jarising@gmail.com>
2018-03-05 10:16:35 +0000

    prep for merge

--- src/optimize-surface.jl
+++ src/optimize-surface.jl
@@ -2,8 +2,7 @@
 
 include("lib/readconfig.jl")
 if !isdefined(:config)
-    #config = readconfig("../configs/single.yml") # Just use 1 year for optimization
-    config = readconfig("../configs/paleo.yml")
+    config = readconfig("../configs/single.yml") # Just use 1 year for optimization
 end
 
 allowreservoirs = true

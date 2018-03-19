7db63e8112f58267da404c6504f3e189cbbde4e7
Laureline <laureline.josset@gmail.com>
2018-03-19 16:21:49 -0400

    analyzing the results

---
+++ analyses/climatevariability/analyze-reservoirs.jl
@@ -0,0 +1,31 @@
+#cd("../../src")
+include("../../src/nui.jl")
+config = readconfig("../configs/standard-50year.yml");
+
+
+# Optimization without reservoirs
+config["rescap"] = "zero";
+include("../../src/optimize-surface.jl");
+writecsv("../analyses/climatevariability/failuresin.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+
+# Optimization with reservoirs at their current capacities
+config["rescap"] = "full";
+
+#cache_clear()
+include("../../src/optimize-surface.jl");
+writecsv("../analyses/climatevariability/failurecon.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+
+include("../../src/simulate.jl")
+
+savedata("../analyses/climatevariability/captures.csv", :Reservoir, :captures)
+
+savedata("../analyses/climatevariability/storagecapmax.csv", :Reservoir, :storagecapacitymax)
+
+savedata("../analyses/climatevariability/storage.csv", :Reservoir, :storage)
+
+
+# Optimization of the capacities
+
+
+
+#cd("../analyses/climatevariability")

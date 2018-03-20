11514191a97e1a754f93598925cdb9dc6cd9b087
Laureline <laureline.josset@gmail.com>
2018-03-20 11:02:50 -0400

    fix perso modification

--- analyses/climatevariability/analyze-optalloc-60.jl
+++ analyses/climatevariability/analyze-optalloc-60.jl
@@ -2,12 +2,12 @@
 include("../../src/nui.jl")
 config = readconfig("../configs/standard-1year.yml");
 config["timestep"] = 1 
-config["startweather"] = 13
+config["startweather"] = 1
 
 
 # Optimization without reservoirs
 config["rescap"] = "zero";
-for yy in 2:60
+for yy in 1:60
 	include("../../src/optimize-waterallocation.jl");
 	writecsv("../analyses/climatevariability/failuresin_optall_year$yy.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
 	writecsv("../analyses/climatevariability/waterfromgw_optall_year$yy.csv", reshape(sol.sol[sum(varlens[1:3])+1:sum(varlens[1:4])], numregions, numsteps))

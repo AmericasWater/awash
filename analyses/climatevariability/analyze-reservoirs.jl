6c20c6212972240235e1aecde8d86cb5bd8b0848
Laureline <laureline.josset@gmail.com>
2018-03-25 20:33:16 -0400

    update

--- analyses/climatevariability/analyze-reservoirs.jl
+++ analyses/climatevariability/analyze-reservoirs.jl
@@ -6,21 +6,21 @@ config["timestep"] = 12
 # Optimization without reservoirs
 config["rescap"] = "zero";
 include("../../src/optimize-surface.jl");
-writecsv("../analyses/climatevariability/failuresin.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+writecsv("../analyses/climatevariability/analyzereservoir/failuresin.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
 
 # Optimization with reservoirs at their current capacities
 config["rescap"] = "full";
 
 #cache_clear()
 include("../../src/optimize-surface.jl");
-writecsv("../analyses/climatevariability/failurecon.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+writecsv("../analyses/climatevariability/analyzereservoir/failurecon.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
 
 include("../../src/simulate.jl")
 
-savedata("../analyses/climatevariability/captures.csv", :Reservoir, :captures)
+savedata("../analyses/climatevariability/analyzereservoir/captures.csv", :Reservoir, :captures)
 
-savedata("../analyses/climatevariability/storagecapmax.csv", :Reservoir, :storagecapacitymax)
+savedata("../analyses/climatevariability/analyzereservoir/storagecapmax.csv", :Reservoir, :storagecapacitymax)
 
-savedata("../analyses/climatevariability/storage.csv", :Reservoir, :storage)
+savedata("../analyses/climatevariability/analyzereservoir/storage.csv", :Reservoir, :storage)
 
 

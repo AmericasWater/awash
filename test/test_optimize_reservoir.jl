d342bbef219899c3b769d2f1d3b868236c8cace4
James Rising <jarising@gmail.com>
2018-04-29 11:36:56 +0100

    should be 3 dims

--- test/test_optimize_reservoir.jl
+++ test/test_optimize_reservoir.jl
@@ -18,9 +18,9 @@ solver = ClpSolver()
 @time sol = houseoptimize(house, solver)
 # Save the results for simulation
 varlens = varlengths(house.model, house.paramcomps, house.parameters)
-serialize(open(datapath("extraction/withdrawals$suffix.jld"), "w"), reshape(sol.sol[varlens[1]+1:sum(varlens[1:2])], numcanals, numsteps))
-serialize(open(datapath("extraction/returns$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:2])+1:sum(varlens[1:3])], numcanals, numsteps))
-serialize(open(datapath("extraction/captures$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:3])+1:end], numreservoirs, numsteps))
+serialize(open(datapath("extraction/withdrawals$suffix.jld"), "w"), reshape(sol.sol[varlens[1]+1:sum(varlens[1:2])], numcanals, numscenarios, numsteps))
+serialize(open(datapath("extraction/returns$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:2])+1:sum(varlens[1:3])], numcanals, numscenarios, numsteps))
+serialize(open(datapath("extraction/captures$suffix.jld"), "w"), reshape(sol.sol[sum(varlens[1:3])+1:end], numreservoirs, numscenarios, numsteps))
 
 # Run the model
 include("../src/model.jl")

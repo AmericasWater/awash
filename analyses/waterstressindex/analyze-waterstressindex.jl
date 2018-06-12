ab9747bf6f550cf58f244a85ed2021d4c2eb89f3
Laureline Josset <lj2390@columbia.edu>
2018-06-12 11:16:09 -0400

    scripts to run optim/sim

--- analyses/waterstressindex/analyze-waterstressindex.jl
+++ analyses/waterstressindex/analyze-waterstressindex.jl
@@ -5,29 +5,116 @@ config = readconfig("../configs/standard-10year.yml");
 
 # Optimization without reservoirs
 config["rescap"] = "zero";
+config["filtercanals"] = "contains";
+config["waterrightconst"] = nothing;
+flowprop = [0. 0.37 0.5]
+
 # Optimization with reservoirs at their current capacities
 #config["rescap"] = "full";
+for ee in 1:length(flowprop)
+    savingresultspath = "../analyses/waterstressindex/10years/"
+    config["proportionnaturalflowforenvironment"] = flowprop[ee]
+    propenv = config["proportionnaturalflowforenvironment"]
+    reval = config["rescap"]
+    if get(config, "filtercanals", nothing) != nothing
+        configname = "surface-propenv$propenv-rescap$reval-nocanal"
+    else
+        configname = "surface-propenv$propenv-rescap$reval"
+    end
 
-include("../../src/optimize-surface.jl");
-writecsv("../analyses/waterstressindex/failure.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+    include("../../src/optimize-surface.jl");
+    writecsv("$savingresultspath/failure-$configname.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
 
-include("../../src/simulate.jl")
+    include("../../src/simulate.jl")
 
-savedata("../analyses/waterstressindex/dem_tot.csv", :WaterDemand, :totaldemand)
-savedata("../analyses/waterstressindex/dem_ir.csv", :WaterDemand, :totalirrigation)
-savedata("../analyses/waterstressindex/dem_do.csv", :WaterDemand, :domesticuse)
-savedata("../analyses/waterstressindex/dem_in.csv", :WaterDemand, :industrialuse)
-savedata("../analyses/waterstressindex/dem_ur.csv", :WaterDemand, :urbanuse)
-savedata("../analyses/waterstressindex/dem_th.csv", :WaterDemand, :thermoelectricuse)
-savedata("../analyses/waterstressindex/dem_li.csv", :WaterDemand, :livestockuse)
+    savedata("$savingresultspath/dem_tot-$configname.csv", :WaterDemand, :totaldemand)
+    #savedata("$savingresultspath/dem_ir.csv", :WaterDemand, :totalirrigation)
+    #savedata("$savingresultspath/dem_do.csv", :WaterDemand, :domesticuse)
+    #savedata("$savingresultspath/dem_in.csv", :WaterDemand, :industrialuse)
+    #savedata("$savingresultspath/dem_ur.csv", :WaterDemand, :urbanuse)
+    #savedata("$savingresultspath/dem_th.csv", :WaterDemand, :thermoelectricuse)
+    #savedata("$savingresultspath/dem_li.csv", :WaterDemand, :livestockuse)
+
+    savedata("$savingresultspath/allocation_wgw-$configname.csv", :Allocation, :watergw)
+    savedata("$savingresultspath/allocation_wsw-$configname.csv", :Allocation, :swsupply)
+    savedata("$savingresultspath/allocation_bal-$configname.csv", :Allocation, :balance)
+
+    savedata("$savingresultspath/wsi_indexgw-$configname.csv", :WaterStressIndex, :indexgw)
+    savedata("$savingresultspath/wsi_indexsw-$configname.csv", :WaterStressIndex, :indexsw)
+    savedata("$savingresultspath/wsi_indexWaSSli-$configname.csv", :WaterStressIndex, :indexWaSSli)
+    savedata("$savingresultspath/wsi_indexWaSSI-$configname.csv", :WaterStressIndex, :indexWaSSI)
+    #savedata("$savingresultspath/wsi_indexWSI.csv", :WaterStressIndex, :indexWSI)
+
+    if get(config, "filtercanals", nothing) != nothing
+        configname = "conj-propenv$propenv-rescap$reval-nocanal"
+    else
+        configname = "conj-propenv$propenv-rescap$reval"
+    end
+    include("../../src/optimize-waterallocation.jl");
+    writecsv("$savingresultspath/failure-$configname.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+
+    include("../../src/simulate.jl")
+
+    savedata("$savingresultspath/dem_tot-$configname.csv", :WaterDemand, :totaldemand)
+    #savedata("$savingresultspath/dem_ir.csv", :WaterDemand, :totalirrigation)
+    #savedata("$savingresultspath/dem_do.csv", :WaterDemand, :domesticuse)
+    #savedata("$savingresultspath/dem_in.csv", :WaterDemand, :industrialuse)
+    #savedata("$savingresultspath/dem_ur.csv", :WaterDemand, :urbanuse)
+    #savedata("$savingresultspath/dem_th.csv", :WaterDemand, :thermoelectricuse)
+    #savedata("$savingresultspath/dem_li.csv", :WaterDemand, :livestockuse)
+
+    savedata("$savingresultspath/allocation_wgw-$configname.csv", :Allocation, :watergw)
+    savedata("$savingresultspath/allocation_wsw-$configname.csv", :Allocation, :swsupply)
+    savedata("$savingresultspath/allocation_bal-$configname.csv", :Allocation, :balance)
+
+    savedata("$savingresultspath/wsi_indexgw-$configname.csv", :WaterStressIndex, :indexgw)
+    savedata("$savingresultspath/wsi_indexsw-$configname.csv", :WaterStressIndex, :indexsw)
+    savedata("$savingresultspath/wsi_indexWaSSli-$configname.csv", :WaterStressIndex, :indexWaSSli)
+    savedata("$savingresultspath/wsi_indexWaSSI-$configname.csv", :WaterStressIndex, :indexWaSSI)
+    #savedata("$savingresultspath/wsi_indexWSI.csv", :WaterStressIndex, :indexWSI)
+end
 
 
-savedata("../analyses/waterstressindex/allocation_wgw.csv", :Allocation, :watergw)
-savedata("../analyses/waterstressindex/allocation_wsw.csv", :Allocation, :swsupply)
-savedata("../analyses/waterstressindex/allocation_bal.csv", :Allocation, :balance)
+config["filtercanals"] = nothing;
+config["waterrightconst"] = "GW";
 
-savedata("../analyses/waterstressindex/wsi_indexgw.csv", :WaterStressIndex, :indexgw)
-savedata("../analyses/waterstressindex/wsi_indexsw.csv", :WaterStressIndex, :indexsw)
-savedata("../analyses/waterstressindex/wsi_indexWaSSli.csv", :WaterStressIndex, :indexWaSSli)
-savedata("../analyses/waterstressindex/wsi_indexWaSSI.csv", :WaterStressIndex, :indexWaSSI)
-#savedata("../analyses/waterstressindex/wsi_indexWSI.csv", :WaterStressIndex, :indexWSI)
+for ee in 1:length(flowprop)
+    savingresultspath = "../analyses/waterstressindex/10years/"
+    config["proportionnaturalflowforenvironment"] = flowprop[ee]
+    propenv = config["proportionnaturalflowforenvironment"]
+    reval = config["rescap"]
+
+    if get(config, "filtercanals", nothing) != nothing
+        configname = "conj-propenv$propenv-rescap$reval-nocanal"
+    else
+        configname = "conj-propenv$propenv-rescap$reval"
+    end
+    if get(config, "waterrightconst", nothing) != nothing
+        waterrightval = config["waterrightconst"]
+        configname = "$configname-rights$waterrightval"
+    end
+
+
+    include("optimize-waterallocation.jl");
+    writecsv("$savingresultspath/failure-$configname.csv", reshape(sol.sol[1:varlens[1]], numregions, numsteps));
+
+    include("simulate.jl")
+
+    savedata("$savingresultspath/dem_tot-$configname.csv", :WaterDemand, :totaldemand)
+    #savedata("$savingresultspath/dem_ir.csv", :WaterDemand, :totalirrigation)
+    #savedata("$savingresultspath/dem_do.csv", :WaterDemand, :domesticuse)
+    #savedata("$savingresultspath/dem_in.csv", :WaterDemand, :industrialuse)
+    #savedata("$savingresultspath/dem_ur.csv", :WaterDemand, :urbanuse)
+    #savedata("$savingresultspath/dem_th.csv", :WaterDemand, :thermoelectricuse)
+    #savedata("$savingresultspath/dem_li.csv", :WaterDemand, :livestockuse)
+
+    savedata("$savingresultspath/allocation_wgw-$configname.csv", :Allocation, :watergw)
+    savedata("$savingresultspath/allocation_wsw-$configname.csv", :Allocation, :swsupply)
+    savedata("$savingresultspath/allocation_bal-$configname.csv", :Allocation, :balance)
+
+    savedata("$savingresultspath/wsi_indexgw-$configname.csv", :WaterStressIndex, :indexgw)
+    savedata("$savingresultspath/wsi_indexsw-$configname.csv", :WaterStressIndex, :indexsw)
+    savedata("$savingresultspath/wsi_indexWaSSli-$configname.csv", :WaterStressIndex, :indexWaSSli)
+    savedata("$savingresultspath/wsi_indexWaSSI-$configname.csv", :WaterStressIndex, :indexWaSSI)
+    #savedata("$savingresultspath/wsi_indexWSI.csv", :WaterStressIndex, :indexWSI)
+end

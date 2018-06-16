1f4613a08c0baf1d22c010632be8fcd6f40dc1bf
James Rising <jarising@gmail.com>
2018-06-16 12:36:58 -0600

    fix reading of paleo reservoirs

--- src/lib/reservoirs.jl
+++ src/lib/reservoirs.jl
@@ -25,7 +25,7 @@ function getreservoirs(config::Union{Dict{Any,Any},Dict{AbstractString,Any}})
     if dataset == "three"
         DataFrame(collection="three", colid=2)
     else
-        reservoirs = readtable(loadpath("reservoirs/allreservoirs.csv"))
+        reservoirs = readtable(loadpath("reservoirs/allreservoirs.csv"), eltypes=[String, String, Float64, Float64, Float64, Float64, Float64])
         if get(config, "filterstate", nothing) != nothing
             reservoirs = reservoirs[floor(reservoirs[:fips] / 1000) .== parse(Int64, config["filterstate"]), :]
         end

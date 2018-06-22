e6b01b91eac0df4c269a4f59f1703e9027ae7758
James Rising <jarising@gmail.com>
2018-06-22 16:55:26 +0100

    prep for office run

--- analyses/landvalue/constrainopt-byprice.jl
+++ analyses/landvalue/constrainopt-byprice.jl
@@ -33,41 +33,46 @@ end
 
 results = DataFrame(filepath=String[], costs=Float64[], objective=Float64[])
 
-for filepath in filepaths
-    mat = readcsv("results/" * filepath)' # Transpose to crop x county
+for switchcost in [0; collect(exp.(linspace(log(1), log(1000), 10)))]
+    for filepath in filepaths
+        mat = readcsv(filepath)' # Transpose to crop x county
 
-    # Parameters: areas for each crop in each county
+        # Parameters: areas for each crop in each county
 
-    # Objectve
-    if switchcost > 0
-        ff = OptiMimi.vectorsingle([size(mat)[1], size(mat)[2]], (ii, jj) -> mat[ii, jj] - switchcost * (baseline[baselinerow[jj], :maxcrop] != String(areacrops[ii])))
-    else
-        ff = OptiMimi.vectorsingle([size(mat)[1], size(mat)[2]], (ii, jj) -> mat[ii, jj])
+        # Objectve
+        if switchcost > 0
+            ff = OptiMimi.vectorsingle([size(mat)[1], size(mat)[2]], (ii, jj) -> mat[ii, jj] - switchcost * (baseline[baselinerow[jj], :maxcrop] != String(areacrops[ii])))
+        else
+            ff = OptiMimi.vectorsingle([size(mat)[1], size(mat)[2]], (ii, jj) -> mat[ii, jj])
+        end
+
+        function areagen(subA, rr)
+            subA[:] = 1
+        end
+        AA = OptiMimi.matrixintersect([size(mat)[2]], [size(mat)[1], size(mat)[2]], [:county], [:crop, :county], areagen)
+        bb = convert(Vector{Float64}, knownareas[:mytotal] * 0.404686) # Convert to Ha
+
+        # Constrain total area per crop to existing
+        for cc in 1:length(areacrops)
+            total = sum(knownareas[areacrops[cc]] * 0.404686)
+            subAA = spzeros(size(mat)[1], size(mat)[2])
+            subAA[cc, :] = 1
+            AA = [AA; vec(subAA)']
+            push!(bb, total)
+        end
+
+        uppers = convert(Vector{Float64}, repeat(knownareas[:mytotal] * 0.404686, inner=6))
+        lowers = zeros(prod(size(mat)))
+        
+        ff[ff .== Inf] = maximum(ff[ff .!= Inf]*10+1e6)
+        ff[isnan.(ff)] = 0
+
+        solver = GurobiSolver()
+        sol = linprog(-ff, AA, '<', bb, lowers, uppers, solver)
+
+        push!(results, [filepath, switchcost, -sol.objval])
+        println(results)
     end
-
-    function areagen(subA, rr)
-        subA[:] = 1
-    end
-    AA = OptiMimi.matrixintersect([size(mat)[2]], [size(mat)[1], size(mat)[2]], [:county], [:crop, :county], areagen)
-    bb = convert(Vector{Float64}, knownareas[:mytotal] * 0.404686) # Convert to Ha
-
-    # Constrain total area per crop to existing
-    for cc in 1:length(areacrops)
-        total = sum(knownareas[areacrops[cc]] * 0.404686)
-        subAA = spzeros(size(mat)[1], size(mat)[2])
-        subAA[cc, :] = 1
-        AA = [AA; vec(subAA)']
-        push!(bb, total)
-    end
-
-    uppers = convert(Vector{Float64}, repeat(knownareas[:mytotal] * 0.404686, inner=6))
-    lowers = zeros(prod(size(mat)))
-
-    ff[ff .== Inf] = maximum(ff[ff .!= Inf]*10+1e6)
-    ff[isnan.(ff)] = 0
-
-    solver = GurobiSolver()
-    sol = linprog(-ff, AA, '<', bb, lowers, uppers, solver)
-
-    push!(results, [filepath, switchcost, -sol.objval])
 end
+
+writetable("constopt-byprice.csv", results)

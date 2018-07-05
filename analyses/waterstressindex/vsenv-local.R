ff8bd07a8c8579bd67c42955bf5854247ea7d947
James Rising <jarising@gmail.com>
2018-07-05 15:20:48 +0100

    clean up

--- analyses/waterstressindex/vsenv-local.R
+++ analyses/waterstressindex/vsenv-local.R
@@ -50,7 +50,9 @@ gg.usmap(df$failurefrac, df$FIPS, df$failurefrac.worst, extra.polygon.aes=aes(si
     scale_colour_gradientn(name="Failure\nFraction", colours=c("#91bfdb", "#ffffe5", "#fe9929", "#662506"), values=c(0, .001, .5, 1), labels = scales::percent, limits=c(0, 1)) +
 coord_map("albers", lat0=39, lat1=45) +
     scale_size(range=c(0, .5)) + guides(size=F) +
-    theme(legend.justification=c(1,0), legend.position=c(1,0))
+    theme(legend.justification=c(1,0), legend.position=c(1,0),
+          panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+          axis.text.x=element_blank(), axis.text.y=element_blank())
 ggsave("failfrac-local.pdf", width=7, height=4)
 
 gg.usmap(df$natflowav, df$FIPS, df$natflowav.worst, extra.polygon.aes=aes(size=1 - borders)) +
@@ -58,5 +60,7 @@ gg.usmap(df$natflowav, df$FIPS, df$natflowav.worst, extra.polygon.aes=aes(size=1
     scale_colour_gradientn(name="Natural\nFlow\nAvailable", colours=c("#fee090", "#fee090", "#e0f3f8", "#74add1", "#313695"), values=c(0, .37, .37001, .685, 1), labels = scales::percent, limits=c(0, 1)) +
     coord_map("albers", lat0=39, lat1=45) +
     scale_size(range=c(0, .5)) + guides(size=F) +
-    theme(legend.justification=c(1,0), legend.position=c(1,0))
+    theme(legend.justification=c(1,0), legend.position=c(1,0),
+          panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+          axis.text.x=element_blank(), axis.text.y=element_blank())
 ggsave("natflowa-local.pdf", width=7, height=4)

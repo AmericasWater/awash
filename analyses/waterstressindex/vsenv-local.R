f569ee4ac04ce406168ee688ef40db8c098616c3
James Rising <jarising@gmail.com>
2018-07-10 09:41:31 +0100

    reorg display

--- analyses/waterstressindex/vsenv-local.R
+++ analyses/waterstressindex/vsenv-local.R
@@ -1,8 +1,5 @@
 setwd("~/research/water/awash/analyses/waterstressindex")
 
-library(ncdf4)
-library(dplyr)
-
 df <- read.csv("../../data/counties/extraction/USGS-2010.csv")
 df <- df %>% left_join(read.csv("../../data/counties/county-info.csv"))
 
@@ -43,34 +40,6 @@ for (ii in 1:nrow(df)) {
     df$natflowav.worst[ii] <- min(natflowav)
 }
 
-source("~/projects/research-common/R/ggmap.R")
-library(png)
-library(grid)
-library(cowplot)
+include("vsenv-lib.R")
 
-gl <- rasterGrob(readPNG("failfrac-legend.png"), interpolate=TRUE)
-gg <- gg.usmap(df$failurefrac, df$FIPS, df$failurefrac.worst, extra.polygon.aes=aes(size=borders)) +
-    scale_fill_gradientn(name="Failure\nFraction", colours=c("#91bfdb", "#ffffe5", "#fe9929", "#662506"), values=c(0, .001, .5, 1), labels = scales::percent, limits=c(0, 1)) +
-    scale_colour_gradientn(name="Failure\nFraction", colours=c("#91bfdb", "#ffffe5", "#fe9929", "#662506"), values=c(0, .001, .5, 1), labels = scales::percent, limits=c(0, 1)) +
-    scale_size(range=c(0, .5)) + guides(size=F) +
-    theme(legend.justification=c(1,0), legend.position=c(1,0),
-          panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
-          axis.text.x=element_blank(), axis.text.y=element_blank(),
-          plot.margin=unit(rep(0, 4), "cm")) + scale_x_continuous(expand=c(0, 0)) + scale_y_continuous(expand=c(0, 0))
-hh <- ggdraw(gg)
-hh + draw_grob(gl, 0.05, 0.02, 0.22, 0.2)
-ggsave("failfrac-local.pdf", width=5.9, height=3.2)
-
-gl <- rasterGrob(readPNG("natflowa-legend.png"), interpolate=TRUE)
-gg <- gg.usmap(df$natflowav, df$FIPS, df$natflowav.worst, extra.polygon.aes=aes(size=1 - borders)) +
-    scale_fill_gradientn(name="Natural\nFlow\nAvailable", colours=c("#fee090", "#fee090", "#abd9e9", "#74add1", "#313695"), values=c(0, .37, .37001, .685, 1), labels = scales::percent, limits=c(0, 1)) +
-    scale_colour_gradientn(name="Natural\nFlow\nAvailable", colours=c("#fee090", "#fee090", "#abd9e9", "#74add1", "#313695"), values=c(0, .37, .37001, .685, 1), labels = scales::percent, limits=c(0, 1)) +
-    scale_size(range=c(0, .5)) + guides(size=F) +
-    theme(legend.justification=c(1,0), legend.position=c(1,0),
-          panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
-          axis.text.x=element_blank(), axis.text.y=element_blank(),
-          plot.margin=unit(rep(0, 4), "cm")) +
-        scale_x_continuous(expand=c(0, 0)) + scale_y_continuous(expand=c(0, 0))
-hh <- ggdraw(gg)
-hh + draw_grob(gl, 0.05, 0.02, 0.22, 0.2)
-ggsave("natflowa-local.pdf", width=5.9, height=3.2)
+plot.failavail(df$fips, df$failurefrac, df$failurefrac.worst, df$natflowav, df$natflowav.worst, "local")

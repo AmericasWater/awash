0213aa0a2b7cd8fb0311af558380ace9dabb7be5
James Rising <jarising@gmail.com>
2018-07-16 12:32:36 +0100

    from laptop'

---
+++ analyses/waterstressindex/stress-annual.R
@@ -0,0 +1,17 @@
+setwd("~/research/water/awash/analyses/waterstressindex")
+
+suffix <- "withres" # "nores"
+
+source("vsenv-lib.R")
+
+df <- read.csv(paste0("stress-annual-", suffix, ".csv"))
+
+supersources <- split.fipsyears(df$supersource, max)
+minefps <- split.fipsyears(df$minefp, min)
+
+demand <- read.csv("../../data/counties/extraction/USGS-2010.csv")
+demand$allsw <- demand$TO_SW * 1383
+
+demand$allsw[demand$allsw == 0] <- 1
+
+plot.failavail(df$fips[1:3109], supersources$median / demand$allsw, supersources$worst / demand$allsw, minefps$median / 100, minefps$worst / 100, paste0("annual-", suffix))

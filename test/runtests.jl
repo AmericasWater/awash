52a8a4398d26a82f2c5cafce4b9944ee991bb9db
James Rising <jarising@gmail.com>
2019-02-17 10:59:35 +0000

    report test times

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Base.Test
+using Dates
 
 function requirepackage(pkg, checkout=false; version=nothing)
     try
@@ -32,12 +33,21 @@ requirepackage("Clp")
 requirepackage("NullableArrays")
 requirepackage("NaNMath")
 
+println("test_optimize_surface.jl: $(Dates.now())")
 include("test_optimize_surface.jl")
+println("test_simulate.jl: $(Dates.now())")
 include("test_simulate.jl")
+println("test_Aquaculture.jl: $(Dates.now())")
 include("test_Aquaculture.jl")
+println("test_Reservoir_optimization.jl: $(Dates.now())")
 include("test_Reservoir_optimization.jl")
+println("test_optimize_reservoir.jl: $(Dates.now())")
 include("test_optimize_reservoir.jl")
+println("test_caching.jl: $(Dates.now())")
 include("test_caching.jl")
+println("test_reservoirs.jl: $(Dates.now())")
 include("test_reservoirs.jl")
+println("test_leapsteps.jl: $(Dates.now())")
 include("test_leapsteps.jl")
+println("test_agsteps.jl: $(Dates.now())")
 include("test_agsteps.jl")

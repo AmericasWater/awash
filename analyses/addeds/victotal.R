88e652b48766ee86fc77da4de43a2ede8bb241bc
James Rising <jarising@gmail.com>
2019-02-21 14:08:30 +0000

    handle winter wheat

---
+++ analyses/addeds/victotal.R
@@ -0,0 +1,74 @@
+setwd("~/research/awash/analyses/addeds")
+load("../../data/counties/waternet/waternet.RData")
+
+library(ncdf4)
+
+ncin <- nc_open("../../data/cache/counties/contributing_runoff_by_gage.nc")
+gages <- ncvar_get(ncin, "gage_id")
+flows <- ncvar_get(ncin, "totalflow")
+areas <- ncvar_get(ncin, "contributing_area")
+
+## Construct gage names
+gage.names <- rep("", 22587)
+for (ii in 2:length(gages))
+    gage.names <- paste(gage.names, strsplit(gages[ii], "")[[1]], sep="")
+gage.names <- gsub("\" *", "", gage.names)
+
+manning.time <- function(dist, drop, flow) {
+    n <- .035 # Major natural rivers from http://www.engineeringtoolbox.com/mannings-roughness-d_799.html
+    slope <- drop / dist
+    radius <- (flow / ((1/n)*(1/2)^(2/3)*sqrt(slope)*(pi/2)))^(1/(2+2/3))
+
+    velocity <- (1 / n) * radius^(2/3) * slope^(1/2)
+    velocity * dist / (24*60*60*30)
+}
+
+add.delay <- function(flow.time, flow.data, dist.m, drop.m, out.time) {
+    if (sum(!is.na(flow.data)) < 2)
+        return(rep(NA, length(out.time)))
+
+    apxfun <- approxfun(flow.time, flow.data)
+    flow.average <- mean(flow.data, na.rm=T)
+    if (is.na(drop.m) || drop.m < 0)
+        dt <- 0
+    else
+        dt <- manning.time(dist.m, drop.m, flow.average)
+    apxfun(out.time - dt)
+}
+
+total.flows <- matrix(NA, nrow(network), dim(flows)[2])
+
+get.total.flows <- function(ii) {
+    if (!is.na(total.flows[ii, 700]))
+        return(total.flows[ii,])
+
+    total.flows[ii,] <<- 0 # fill 0 in case recurse
+    
+    alltime <- 1:dim(flows)[2]
+
+    incoming <- rep(0, dim(flows)[2])
+    for (jj in which(network$nextpt == ii))
+        incoming <- incoming + add.delay(alltime, get.total.flows(jj), network$dist[jj], network$elev[jj] - network$elev[ii], alltime)
+
+    name <- paste(network$collection[ii], network$colid[ii], sep='.')
+    gg <- which(gage.names == name)
+    if (length(gg) != 1) {
+        print(c(name, gg))
+        contribs <- rep(0, dim(flows)[2])
+    } else {
+        contribs <- flows[gg,] * areas[gg] * 1000 / (24*60*60*30)
+        contribs[is.nan(contribs)] <- 0 # it happens
+    }
+
+    total.flows[ii,] <<- contribs + incoming
+    return(total.flows[ii,])
+}
+
+for (ii in 1:nrow(network)) {
+    if (is.na(total.flows[ii, 700])) {
+        print(c(ii, mean(!is.na(total.flows[, 700]))))
+        get.total.flows(ii)
+    }
+}
+
+save(total.flows, file="victotal.RData")
---
+++ analyses/landvalue/tempdists.R
@@ -0,0 +1,72 @@
+setwd("~/research/awash/analyses/landvalue")
+
+library(dplyr)
+
+maps <- c("constopt-currentprofits-pfixmo-chirr.csv", "constopt-all2050profits-pfixmo-notime-histco.csv",
+          "constopt-all2070profits-pfixmo-notime-histco.csv")
+tcol <- c('bio1_mean', 'bio1_2050', 'bio1_2070')
+sname <- c('Current', '2050', '2070')
+
+df <- read.csv("~/Dropbox/Agriculture Weather/us-bioclims-new.csv")
+for (year in c(2050, 2070)) {
+    sumbio1 <- rep(0, nrow(df))
+    countmodels <- 0
+    for (model in c("ac", "bc", "cc", "cn", "gf", "gs",
+                    "hd", "he", "hg", "in", "ip", "mc",
+                    "mg", "mi", "mp", "mr" , "no")) {
+        moddf <- read.csv(paste0("~/Dropbox/Agriculture Weather/bioclims-", year, "/", model, "85bi", year - 2000, ".csv"))
+        sumbio1 <- sumbio1 + moddf$bio1_mean
+        countmodels <- countmodels + 1
+    }
+
+    df[, paste0("bio1_", year)] <- sumbio1 / countmodels
+}
+df$fips <- df$STATE * 100 + df$COUNTY / 10
+
+results <- data.frame()
+for (scenario in 1:3) {
+    topcrops <- read.csv(paste0("results/", maps[scenario]))
+    topcrops <- topcrops %>% left_join(df)
+    ## Determine max crop by 1 C bins
+    
+    topcrops$bin <- round(topcrops[, tcol[scenario]] / 5) / 2
+    for (bin in unique(topcrops$bin))
+        results <- rbind(results, data.frame(scenario=sname[scenario], bin, count=sum(topcrops$bin == bin), density=mean(topcrops$bin == bin), topcrop=names(which.max(table(topcrops$topcrop[topcrops$bin == bin])))))
+}
+
+library(ggplot2)
+
+## breaks <- c()
+## values <- c()
+## if ('Barley' %in% results$topcrop) {
+##     breaks <- 'Barley'
+##     values <- '#f8766d'
+## }
+## if ('Cotton' %in% results$topcrop) {
+##     breaks <- c(breaks, 'Cotton')
+##     values <- c(values, '#00ba38')
+## }
+## if ('Corn' %in% results$topcrop) {
+##     breaks <- c(breaks, 'Corn')
+##     values <- c(values, '#b79f00')
+## }
+## if ('Rice' %in% results$topcrop) {
+##     breaks <- c(breaks, 'Rice')
+##     values <- c(values, '#00bfc4')
+## }
+## if ('Soybean' %in% results$topcrop) {
+##     breaks <- c(breaks, 'Soybean')
+##     values <- c(values, '#619cff')
+## }
+## if ('Wheat' %in% results$topcrop) {
+##     breaks <- c(breaks, 'Wheat')
+##     values <- c(values, '#f564e3')
+## }
+
+ggplot(results, aes(bin, count, fill=as.character(topcrop))) +
+    facet_grid(scenario ~ .) +
+    geom_bar(stat="identity") + theme_bw() +
+    xlab("Temperature (C)") + ylab("Counties with average temperature") +
+    scale_fill_discrete(name=NULL)
+##scale_fill_manual(breaks=breaks, values=values)
+ggsave("tempdists.pdf", width=6, height=4)

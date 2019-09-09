99a24d6b26b019ae5615adfa11c00069a522e3e2
Laureline <laureline.josset@gmail.com>
2019-09-09 08:42:17 -0400

    functions input variables removed

--- src/optimize-surface.jl
+++ src/optimize-surface.jl
@@ -33,7 +33,7 @@ summarizeparameters(house, sol.sol)
 #constdf = constraining(house, sol.sol)
 
 # Save the results
-save_optimization_given(house, sol, allowgw=false, allowreservoirs=allowreservoirs)
+save_optimization_given(house, sol, false, allowreservoirs)
 
 # How much water is in the streams?
 values = getconstraintsolution(house, sol, :outflows)
